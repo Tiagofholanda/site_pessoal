006d32c64faf189261e7a72aa3d7b7ae1b170965
tiagofholanda <tfholanda@gmail.com>
2026-08-15 00:13:47 -0300

    Redesenha area restrita.

---
+++ _next/static/zwXW7V9Df8BipRy3Tywdl/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F[locale]","\u002F[locale]\u002Fcurriculo","\u002F[locale]\u002Flogin","\u002F[locale]\u002Fprivacidade","\u002F[locale]\u002Fprojetos-privados","\u002F[locale]\u002Ftermos"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
